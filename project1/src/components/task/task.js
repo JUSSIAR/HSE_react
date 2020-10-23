50bbd56fcff34ae64ab3e32e3fa8af8cf24121ff
JUSSIAR <klokov.stas.022@mail.ru>
2020-10-24 00:54:17 +0300

    fixed comments for hw3 in hw4

--- project1/src/components/task/task.js
+++ project1/src/components/task/task.js
@@ -2,17 +2,15 @@ import React from 'react'
 import './taskStyle.css'
 import MyButton from '../button/button.js'
 
-const completed_status = ["In progress...", "Done"];
-
 const Task = ({props, click}) => {
   return (
-    <div className = {(props.completed == 1) ? "task_item1" : "task_item2"}>
+    <div className = {(props.completed == 1) ? "taskDone" : "taskInProgress"}>
       <div className = "block1">
         <h2 className = "header2"> {props.name} </h2>
         <p className = "description"> {props.description} </p>
       </div>
       <div className = "block2">
-        <p className = "status"> {completed_status[props.completed]} </p>
+        <p className = "status"> {(props.completed == 0) ? "In progress..." : "Done"} </p>
         <MyButton 
           onClick = {click}
           index = {props.id}
