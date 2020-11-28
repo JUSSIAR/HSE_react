fcd5e3e142bc870c4cab1308a6e93916cb000da9
JUSSIAR <klokov.stas.022@mail.ru>
2020-11-28 23:35:40 +0300

    added cond-styles with classnames

--- project1/src/components/task/task.js
+++ project1/src/components/task/task.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import './taskStyle.scss'
+
+import classNames from 'classnames/bind'
+import styles from './taskStyleCompl.module.scss'
+
 import MyButton from '../button/button.js'
 import logo from '../../images/orig.png'
 
+const cx = classNames.bind(styles);
+
+const DONE = "Done";
+const PROGR = "InProgress";
+
 const Task = ({props, click, onClick}) => {
+  const STYLE = ((props.completed === 1) 
+    ? DONE
+    : PROGR
+  )
   return (
-    <div className = {(props.completed == 1) ? "taskDone" : "taskInProgress"}>
+    <div className = {
+      // (props.completed === 1) ? "taskDone" : "taskInProgress"
+      // cx("task", 
+      //   {`task${DONE}` : props.completed === 1},
+      //   {`task${PROGR}` : props.completed === 0}
+      // )
+      cx("task", `task${STYLE}`)
+    }>
       <img 
         id="ready"
         src={logo}
